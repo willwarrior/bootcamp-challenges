38ed6f8cdf9bfebc61cf3be99f6c6198d0c28682
Wilfredo Castaneda <willwarrior@users.noreply.github.com>
2021-12-02 12:34:44 -0500

    Update 3SUM-Medium.js

--- Week2/3SUM-Medium.js
+++ Week2/3SUM-Medium.js
@@ -1,1 +1,19 @@
+function threeSums(numbers) {
+    const sets = [];
 
+    for (let i = 0;i < numbers.length -2; i++) {
+        for (let j = j + 1; k < numbers.length - 1; j++) {
+            for (let k = j + 1; k < numbers.length; k++) {
+                const a = numbers[i];
+                const b = numbers[j];
+                const c = numbers[k];
+                const set = '${a} ${b} ${c}';
+                if (a + b + c === 0 &&
+                    sets.indexOf(set === -1) {
+                        sets.push(set);
+                 }
+            }
+        }
+    }
+    return sets;
+}
